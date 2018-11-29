849b1a229a3e1c9635c4c9604c3c80a396db7e5f
Rafael Paz <rrafaelpaz@gmail.com>
2018-11-30 10:14:45 +1100

    first commit

---
+++ src/test/java/com/microservice/eureka/naming/service/eurekanamingservice/EurekaNamingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservice.eureka.naming.service.eurekanamingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaNamingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
